e905ddbebcdaddf9f76f9f264bb58cb80b77179c
sergiiohuertas <ser_g@hotmail.es>
2024-11-11 17:45:37 +0100

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [tailwind()]
 });
